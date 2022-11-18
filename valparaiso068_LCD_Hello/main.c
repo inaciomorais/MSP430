800478110f81d33095650944e4206fd120fc635b
A. Inácio Morais <69253881+inaciomorais@users.noreply.github.com>
2022-11-18 20:32:51 -0300

    Update main.c

--- valparaiso068_LCD_Hello/main.c
+++ valparaiso068_LCD_Hello/main.c
@@ -43,21 +43,14 @@ void clock_config(void)
  *
  * HITACHI HD44780U ROM A02 Pattern (Datasheet page 18)
  *
- *   LCDPutChar(0x9A); // Ω      LCDPutChar(0xAB); // <<
- *   LCDPutChar(0xBB); // >>     LCDPutChar(0xB0); // °
- *   LCDPutChar(0xC0); // À      LCDPutChar(0xE0); // à
- *   LCDPutChar(0xC1); // Á      LCDPutChar(0xE1); // á
- *   LCDPutChar(0xC2); // Â      LCDPutChar(0xE2); // â
- *   LCDPutChar(0xC3); // Ã      LCDPutChar(0xE3); // ã
- *   LCDPutChar(0xC9); // É      LCDPutChar(0xE9); // é
- *   LCDPutChar(0xCA); // Ê      LCDPutChar(0xEA); // ê
- *   LCDPutChar(0xCD); // Í      LCDPutChar(0xED); // í
- *   LCDPutChar(0xD3); // Ó      LCDPutChar(0xF3); // ó
- *   LCDPutChar(0xD4); // Ô      LCDPutChar(0xF4); // ô
- *   LCDPutChar(0xD5); // Õ      LCDPutChar(0xF5); // õ
- *   LCDPutChar(0xF6); // ö      LCDPutChar(0xC7); // Ç
- *   LCDPutChar(0xE7); // ç      LCDPutChar(0xDA); // Ú
- *   LCDPutChar(0xFA); // ú      LCDPutChar(0xFC); // ü
+ *   LCDPutChar(0x9A); // Ω      LCDPutChar(0xAB); // <<     LCDPutChar(0xBB); // >>     LCDPutChar(0xB0); // °
+ *   LCDPutChar(0xC0); // À      LCDPutChar(0xE0); // à      LCDPutChar(0xC1); // Á      LCDPutChar(0xE1); // á
+ *   LCDPutChar(0xC1); // Á      LCDPutChar(0xE1); // á      LCDPutChar(0xC2); // Â      LCDPutChar(0xE2); // â
+ *   LCDPutChar(0xC3); // Ã      LCDPutChar(0xE3); // ã      LCDPutChar(0xC9); // É      LCDPutChar(0xE9); // é
+ *   LCDPutChar(0xCA); // Ê      LCDPutChar(0xEA); // ê      LCDPutChar(0xCD); // Í      LCDPutChar(0xED); // í
+ *   LCDPutChar(0xD3); // Ó      LCDPutChar(0xF3); // ó      LCDPutChar(0xD4); // Ô      LCDPutChar(0xF4); // ô
+ *   LCDPutChar(0xD5); // Õ      LCDPutChar(0xF5); // õ      LCDPutChar(0xF6); // ö      LCDPutChar(0xC7); // Ç
+ *   LCDPutChar(0xE7); // ç      LCDPutChar(0xDA); // Ú      LCDPutChar(0xFA); // ú      LCDPutChar(0xFC); // ü
  *
  * WINSTAR WH1602C English and European standard font (ET) - See Extended ASCII table
  */
