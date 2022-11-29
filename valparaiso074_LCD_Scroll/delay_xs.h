cf22bfb110104c232065fb1815aa1ae4a63dd480
A. Inácio Morais <69253881+inaciomorais@users.noreply.github.com>
2022-11-28 21:03:01 -0300

    Add files via upload

---
+++ valparaiso074_LCD_Scroll/delay_xs.h
@@ -0,0 +1,31 @@
+/*
+ * delay_xs.h
+ *
+ * 11/2022
+ *
+ * A. Inácio Morais
+ * anderson.morais@protonmail.com - inacio.morais@protonmail.com
+ * +55 35 99161-9878
+ *
+ */
+
+
+//***** Header Files **********************************************************
+#include <msp430.h>
+
+#ifndef DELAY_XS_H_
+#define DELAY_XS_H_
+
+
+//***** Prototypes ************************************************************
+void delay_us(unsigned int us, char MHz);
+void delay_ms(unsigned int ms, char MHz);
+
+
+//***** Defines ***************************************************************
+
+
+//***** Global Variables ******************************************************
+
+
+#endif /* DELAY_XS_H_ */
