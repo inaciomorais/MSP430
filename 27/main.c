ef0b011e8f0cc3969fe0c4b0eadf188960ff59d4
A. Inácio Morais <69253881+inaciomorais@users.noreply.github.com>
2022-11-29 22:20:07 -0300

    Add files via upload

---
+++ 27/main.c
@@ -0,0 +1,17 @@
+#include <msp430.h> 
+
+#define RED_LED_ON 0x01        // P1.0 is the RED LED
+
+void main(void)
+{
+    //WDTCTL = WDTPW | WDTHOLD;   // Stop Watchdog Timer
+
+    P1DIR |= RED_LED_ON;
+    P1OUT |= RED_LED_ON;
+
+    while(1)
+    {
+        WDTCTL = WDTPW | WDTCNTCL;
+    }
+
+}
